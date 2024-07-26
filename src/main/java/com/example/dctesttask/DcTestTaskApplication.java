997f9081e88546d96008ee2e1f4b4cff8f54ad08
Anastasiya <test@mail.test>
2024-07-26 16:15:57 +0300

    create basic spring boot app

---
+++ src/main/java/com/example/dctesttask/DcTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.dctesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dctesttask/DcTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dctesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
